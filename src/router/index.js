a415ad98347725412b62064be4e85c8414c49d02
Jose Iglesias <jogmenz@gmail.com>
2021-02-09 13:49:02 +0000

    Problema con el Login Solucionado

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,7 @@ const routes = [
   },
   {
     path: '/products/categories/:slug',
-    name: 'CategoryProduct',
-    meta: {
-      requiresVisitor: true
-    },
+    name: 'CategoryProduct', 
     // route level code-splitting
     // this generates a separate chunk (productscategories.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
